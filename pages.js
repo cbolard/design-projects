f5375a607bbcd719e227093b82172eeb2aacf000
root <root@debian>
2021-05-29 17:22:46 +0200

    plant life

--- pages.js
+++ pages.js
@@ -1,85 +1,104 @@
-// this is the data for holding which page we're on
-let pageNumber = 0
+//relevant tags
+const nextTag = document.querySelector("footer img.next")
+const prevTag = document.querySelector("footer img.prev")
+const randomTag = document.querySelector("footer img.random")
+const outputTag = document.querySelector("h2")
+const backgroundColor = document.querySelector("body")
+const circleTag = document.querySelector("section div.circle")
 
-// have the content for these pages
+
+//this is the data for holding the page we on
+let pageNumber = 0;
+
+
+//haev the content for the pages
 const pages = [
-  {copy: 'a Brooklyn-based graphic designer', background: '#edc7a9', circle: '#3e78ed'},
-  {copy: "Kanye West's biggest fan", background: '#a1fffe', circle: '#e34a47'},
-  {copy: 'looking for a job at the start of October', background: '#d3c7f3', circle: '#f7fe00'},
-  {copy: `letting you <a href="pdf.pdf">download her PDF</a>`, background: '#faffb8', circle: '#b472e6' }
+    {
+	copy : `<a href ="#">Plant life<a>`,
+	background : "#edc7a9",
+	circle : "#3e78ed",
+    },
+    {
+	copy : `<a href ="#">Type Checker<a>`,
+	background : "#a1fffe",
+	circle : "#e34a47",
+    },
+    {
+	copy : `<a href ="#">Jenna Buchholzz<a>`,
+	background : "#d3c7f3",
+	circle : "#f7fe00",
+    },
+
+    {
+	copy :  `<a href ="#">Brux<a>`,
+	background : "#faffb8",
+	circle : "#b472e6",
+    },
+    {
+	copy :  `<a href ="#">Words of Womxn<a>`,
+	background : "#2b2b2b",
+	circle : "#19b591",
+    },
+
+
 ]
 
-// pick the relevant tags
-const nextTag = document.querySelector('footer img.next')
-const previousTag = document.querySelector('footer img.prev')
-const randomTag = document.querySelector('footer img.random')
-const outputTag = document.querySelector('h2')
-const circleTag = document.querySelector('section div.circle')
-const bodyTag = document.querySelector('body')
+// make a next fuction to increase page Number
+const next = function () {
+    pageNumber += 1
 
-// make a next function to increase the pageNumber
-const next = function() {
-  pageNumber = pageNumber + 1
-
-  if (pageNumber > pages.length - 1) {
-    pageNumber = 0
-  }
-
-  updateSection()
+    if (pageNumber > pages.length - 1) {
+	pageNumber = 0
+    }
+    updateSection()
 }
 
-// make a previous function to decrease the pageNumber
-const previous = function() {
-  pageNumber = pageNumber - 1
+// make a prev fuction to increase page Number
+const prev = function () {
+    pageNumber -= 1
 
-  if (pageNumber < 0) {
-    pageNumber = pages.length - 1
-  }
-
-  updateSection()
+    if (pageNumber < 0) {
+	pageNumber = pages.length - 1
+    }
+    updateSection()
 }
 
-// pick a random slide
-const random = function() {
-  pageNumber = Math.floor(Math.random() * pages.length)
-
-  updateSection()
+//random slide
+const random = function () {
+    pageNumber = Math.floor(Math.random() * pages.length)
+    updateSection()
 }
 
-// this will update the section's content and style
-const updateSection = function() {
-  outputTag.innerHTML = pages[pageNumber].copy
-  circleTag.style.backgroundColor = pages[pageNumber].circle
-  bodyTag.style.backgroundColor = pages[pageNumber].background
+
+// Text HTML to say which page Number
+const updateSection = function () {
+    outputTag.innerHTML = pages[pageNumber].copy
+    backgroundColor.style.backgroundColor =pages[pageNumber].background
+    circleTag.style.backgroundColor =pages[pageNumber].circle
 }
 
-// on click of nextTag, run this
-nextTag.addEventListener('click', function() {
-  next()
-})
-
-// on click of previousTag, run this
-previousTag.addEventListener('click', function() {
-  previous()
-})
-
-// on click of randomTag, run this
-randomTag.addEventListener('click', function() {
-  random()
-})
-
-// when a user presses a key, check for arrow left or right
-// and do next or prev correctly
-document.addEventListener('keyup', function(event) {
-  console.log(event)
-
-  // if the key being pressed is ArrowRight
-  if (event.key == 'ArrowRight') {
+//onClick next tag
+nextTag.addEventListener('click', function (e) {
     next()
-  }
+})
 
-  // if the key being pressed is ArrowLeft
-  if (event.key == 'ArrowLeft') {
-    previous()
-  }
+//onClick prev tag
+prevTag.addEventListener('click', function (e) {
+    prev()
+})
+//onClick random tag
+randomTag.addEventListener('click', function (e) {
+    random()
+})
+
+//when a user presses a key, check arrow left or right
+
+//do next or prev correctly
+document.addEventListener("keyup", function (e) {
+    if(e.key == "ArrowRight"){
+	next()
+    }
+    else if (e.key == "ArrowLeft"){
+	prev()
+    }
 })
